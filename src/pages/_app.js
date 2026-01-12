8c053ac4b219240ead78d03f2622e3ca72de224f
meramarketstore-commits <meramarket.store@gmail.com>
2026-01-12 13:02:19 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,62 +1,62 @@
-import { Montserrat } from 'next/font/google';
-import { useEffect, useState } from 'react';
-import Link from 'next/link';
-import dynamic from 'next/dynamic';
+import { Montserrat } from "next/font/google";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import dynamic from "next/dynamic";
 
-import { PopupProvider } from '@/context/PopupContext';
+import "@/styles/critical.css";
 
-import '@/styles/critical.css';
+import SEOHead from "../components/SEOHead";
+import Header from "../components/Header";
+import { PopupProvider } from "@/context/PopupContext";
 
-import SEOHead from '../components/SEOHead';
-import Header from '../components/Header';
-
-const Footer = dynamic(() => import('../components/Footer'), { ssr: false });
-const DelayedPopup = dynamic(() => import('../components/DelayedPopup'), { ssr: false });
+const Footer = dynamic(() => import("../components/Footer"), { ssr: false });
+const DelayedPopup = dynamic(() => import("../components/DelayedPopup"), {
+  ssr: false,
+});
 
 const montserrat = Montserrat({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  display: 'swap',
-  variable: '--font-montserrat',
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
+  variable: "--font-montserrat",
   preload: true,
 });
 
 export default function MyApp({ Component, pageProps }) {
   const [showButton, setShowButton] = useState(false);
 
+  /* Load NON-CRITICAL assets after first paint */
   useEffect(() => {
     requestIdleCallback(() => {
-      import('bootstrap/dist/css/bootstrap.min.css');
-      import('@/styles/globals.css');
-      import('@/styles/noncritical.css');
-      import('@/styles/DelayedPopup.css');
+      import("bootstrap/dist/css/bootstrap.min.css");
+      import("@/styles/globals.css");
+      import("@/styles/noncritical.css");
+      import("@/styles/DelayedPopup.css");
     });
   }, []);
 
   useEffect(() => {
     const onScroll = () => setShowButton(window.scrollY > 100);
-    window.addEventListener('scroll', onScroll, { passive: true });
+    window.addEventListener("scroll", onScroll, { passive: true });
     onScroll();
-    return () => window.removeEventListener('scroll', onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
   }, []);
 
   return (
     <PopupProvider>
-      <>
-        <SEOHead />
-        <div className={`${montserrat.className} ${montserrat.variable}`}>
-          <Header />
-          <Component {...pageProps} />
-          <Footer />
-          <DelayedPopup />
-        </div>
+      <SEOHead />
+      <div className={`${montserrat.className} ${montserrat.variable}`}>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+        <DelayedPopup />
+      </div>
 
-        {showButton && (
-          <Link href="/join-free-demo-class" legacyBehavior>
-            <a className="sticky-demo-button">Get a Free Demo</a>
-          </Link>
-        )}
-      </>
+      {showButton && (
+        <Link href="/join-free-demo-class" legacyBehavior>
+          <a className="sticky-demo-button">Get a Free Demo</a>
+        </Link>
+      )}
     </PopupProvider>
   );
 }
